98f344bbd9818ed11abdc96fdfe8c3108c5a4299
unknown <nhussaini@luc.edu>
2019-02-20 11:10:57 -0800

    Inspection DAO in progress

--- src/com/fms/dal/inspection/InspectionDAO.java
+++ src/com/fms/dal/inspection/InspectionDAO.java
@@ -1,5 +1,52 @@
 package com.fms.dal.inspection;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import com.fms.dal.DBConnect;
+import com.fms.model.facility.Phone;
+import com.fms.model.inspection.Inspection;
+
 public class InspectionDAO {
+	
+	//Insert inspection informations
+	public Set<Inspection> insertInspectionInfo(Set<Inspection> inspections){
+		Connection connection = DBConnect.getDatabaseConnection();
+		try {
+			Statement insertStatement=connection.createStatement();
+			
+			Iterator<Inspection> inspectionIterator=inspections.iterator();
+			while(inspectionIterator.hasNext()) {
+				
+				Inspection currentInspection=inspectionIterator.next();
+				
+				String insertQuery="INSERT INTO inspection(InspectionID,DateFrom,DateTo,UserID,FacilityID,InspectionType) "
+						+"VALUES('"+currentInspection.getInspectionID()+"','"+currentInspection.getDateFrom()+"','"+currentInspection.getDateTo()+"','"+currentInspection.getUserID()+"','"+currentInspection.getFacilityID()+"','"+currentInspection.getInspectionType()+"')";
+				insertStatement.executeUpdate(insertQuery);
+			}
+		}catch(SQLException se) {
+			se.printStackTrace();
+		}finally {
+			if(connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {}
+			}
+		}
+		return inspections;
+	}
+	
+	
+	//retrieve all inspections for a facility
+	public Set getBuildingInspections(String id) {
+		Connection connection = DBConnect.getDatabaseConnection();
+		Set<Inspection> inspections = new HashSet<>();
+		
+	}
 
 }
